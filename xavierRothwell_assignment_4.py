f41e3776cf7a6b1e4920c3d85bf18883858c5750
XavierR2d2 <xwrothwell@aggies.ncat.edu>
2025-09-26 17:34:00 -0400

    Update xavierRothwell_assignment_4.py
    Added information for test case 1

--- xavierRothwell_assignment_4.py
+++ xavierRothwell_assignment_4.py
@@ -4,3 +4,8 @@ study_hours = int(23)
 social_points = int(35)
 stress_level = int(74)
 
+#Added after Commit 1
+Level_choice = input()
+Easy = ("Easy Difficulty: 12 Credits")
+Medium = ("Medium Difficulty: 15 Credits")
+Hard = ("Hard Difficulty: 18 Credits")
