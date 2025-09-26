49960902627b2afbebd1505698e18ab915b1a4b4
XavierR2d2 <xwrothwell@aggies.ncat.edu>
2025-09-26 17:35:11 -0400

    Update xavierRothwell_assignment_4.py
    Added information for commit 2

--- xavierRothwell_assignment_4.py
+++ xavierRothwell_assignment_4.py
@@ -9,3 +9,9 @@ Level_choice = input()
 Easy = ("Easy Difficulty: 12 Credits")
 Medium = ("Medium Difficulty: 15 Credits")
 Hard = ("Hard Difficulty: 18 Credits")
+
+#Added after Commit 2
+Route_Option1 = ["Play Outside in the Snow with friends"]
+Route_Option2 = ["Study for your mid Term exam, that is tomorrow"]
+Route_Option3 = ["Study for a couple hours then go play in the snow"]
+Route_Option4 = ["Play Outside all day with your friends then study all night "]
